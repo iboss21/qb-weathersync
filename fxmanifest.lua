a2e6c85781a897d2257ea3d9dd77ed1c3ebc64c2
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-03-21 11:27:36 -0600

    Update to new Framework

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,8 @@ version '2.0.0'
 
 shared_scripts {
 	'config.lua',
-	--'@qb-core/shared/locale.lua',
-	--'locales/en.lua'
+	'@qb-core/shared/locale.lua',
+	'locales/en.lua'
 }
 
 server_script 'server/server.lua'
